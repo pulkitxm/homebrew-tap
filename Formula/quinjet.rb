a98494f19f6ebc7b583274acf6c98cdb2a257e2f
Pulkit <kpulkit15234@gmail.com>
2026-08-20 21:22:12 +0530

    Update the Quinjet formula to v0.0.31

---
+++ Formula/quinjet.rb
@@ -0,0 +1,54 @@
+class Quinjet < Formula
+  desc "Fast, live, keyboard-first Git source-control interface for the terminal"
+  homepage "https://quinjet.pulkit.page/"
+  version "0.0.31"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/pulkitxm/quinjet/releases/download/v#{version}/quinjet-macos-aarch64",
+          verified: "github.com/pulkitxm/quinjet/"
+      sha256 "8d9ef2328a9bed958550c9d5e71bc7e952a3a24600d9bf95a658ab0705e17065"
+    end
+    on_intel do
+      url "https://github.com/pulkitxm/quinjet/releases/download/v#{version}/quinjet-macos-x86_64",
+          verified: "github.com/pulkitxm/quinjet/"
+      sha256 "c699a9827c549dd8cf7c9a534b3a17a57253ec78d03a74fe561ae89efbb32e12"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/pulkitxm/quinjet/releases/download/v#{version}/quinjet-linux-aarch64",
+          verified: "github.com/pulkitxm/quinjet/"
+      sha256 "fa8b4bd224848650dd55da55cf97ad0547186916f6eeb2de97fb1bd1ea9c9437"
+    end
+    on_intel do
+      url "https://github.com/pulkitxm/quinjet/releases/download/v#{version}/quinjet-linux-x86_64",
+          verified: "github.com/pulkitxm/quinjet/"
+      sha256 "059640ea803dd5308d9e1e8e114583e34dbfc7b3974554a00e0849d14854f6a2"
+    end
+  end
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on "git"
+
+  def install
+    bin.install Dir["quinjet-*"].fetch(0) => "quinjet"
+    chmod 0555, bin/"quinjet"
+    bin.install_symlink bin/"quinjet" => "q"
+    generate_completions_from_executable(bin/"quinjet", "completions", shells: [:bash, :zsh, :fish])
+    (man1/"quinjet.1").write Utils.safe_popen_read(bin/"quinjet", "man")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/quinjet --version")
+    assert_match version.to_s, shell_output("#{bin}/q --version")
+    system "git", "init", testpath/"repository"
+    assert_match "branch", shell_output("#{bin}/quinjet -C #{testpath}/repository status --json")
+  end
+end
